a5f949bce3a0741b5132c1d4e69d28e9347fd9d8
Morgan Kenyon <morganwkenyon@gmail.com>
2019-07-14 14:57:56 -0500

    Starting to write an integration test.

---
+++ src/TinRoll.Integration/QuestionIntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TinRoll.Integration
+{
+    public class QuestionIntegrationTests
+        :  IClassFixture<WebApplicationFactory<Server.Startup>>
+    {
+        private readonly WebApplicationFactory<Server.Startup> _factory;
+        public QuestionIntegrationTests(WebApplicationFactory<Server.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/api/Question")]
+        public async Task Get_EndpointsTestSuccessResponse(string url)
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("application/json", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
--- src/TinRoll.Server/Program.cs
+++ src/TinRoll.Server/Program.cs
@@ -12,11 +12,14 @@ namespace TinRoll.Server
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
+            CreateWebHostBuilder(args)
+                .Build();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseConfiguration(new ConfigurationBuilder()
                     .AddCommandLine(args)
                     .Build())
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
     }
 }
