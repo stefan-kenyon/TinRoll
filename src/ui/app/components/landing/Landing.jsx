bddb7ee00f6eacf32760d7e6af183e4d9e674ccd
Stefan <stefan.kenyon@gmail.com>
2019-09-01 15:45:54 -0500

    Adding branch to github

--- src/ui/app/components/landing/Landing.jsx
+++ src/ui/app/components/landing/Landing.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 class Landing extends React.Component {
+    constructor(props) {
+        super(props)
+    }
     render() {
         return (
             <div id='tinLanding'>
